d3f7648f018fa794606ce59ab1b9adca23258319
Marcin <marcinszydelko@yahoo.com>
2020-04-15 04:24:52 +0100

    a

--- andromedastrid-site/gatsby-config.js
+++ andromedastrid-site/gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: process.env.API_URL='http://localhost:1337',
+        apiURL: process.env.API_URL='http://0f99cb09.ngrok.io/',
         contentTypes: [
           'users',
         ],
@@ -39,6 +39,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
