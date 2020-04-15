666e4f7786378aae45fbdb71ffe9b2b050ceed5f
Marcin <marcinszydelko@yahoo.com>
2020-04-15 02:47:00 +0100

    connect strapi with gatsby

--- andromedastrid-site/gatsby-config.js
+++ andromedastrid-site/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Andromedastrid`,
+    description: `Andromedastrid Ilustration`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -12,6 +12,15 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+    },
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        contentTypes: [
+          'user'
+        ]
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -29,6 +38,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
